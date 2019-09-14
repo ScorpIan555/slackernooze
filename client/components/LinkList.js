6c7e3ab3c6d37597516d4a4b435203dfa4d8e0cf
ScorpIan555 <ian.daley23@gmail.com>
2019-09-14 16:35:46 -0400

    refactored LinkList into functional component

--- client/components/LinkList.js
+++ client/components/LinkList.js
@@ -15,26 +15,24 @@ const FEED_QUERY = gql`
   }
 `;
 
-class LinkList extends Component {
-  render() {
-    return (
-      <Query query={FEED_QUERY}>
-        {({ loading, error, data }) => {
-          if (loading) return <div>Fetching</div>;
-          if (error) return <div>Error</div>;
+const LinkList = props => {
+  return (
+    <Query query={FEED_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <div>Fetching</div>;
+        if (error) return <div>Error</div>;
 
-          const linksToRender = data.feed.links;
-          return (
-            <div>
-              {linksToRender.map(link => (
-                <NewsLink key={link.id} link={link} />
-              ))}
-            </div>
-          );
-        }}
-      </Query>
-    );
-  }
-}
+        const linksToRender = data.feed.links;
+        return (
+          <div>
+            {linksToRender.map(link => (
+              <NewsLink key={link.id} link={link} />
+            ))}
+          </div>
+        );
+      }}
+    </Query>
+  );
+};
 
 export default LinkList;
