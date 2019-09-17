9391dab368cf1a8f4d181e0722fe075198c305c0
ScorpIan555 <ian.daley23@gmail.com>
2019-09-17 17:22:39 -0400

    cleaned up env files

--- server/src/config/index.js
+++
@@ -1,8 +0,0 @@
-// config.js
-const dotenv = require('dotenv');
-dotenv.config();
-module.exports = {
-  endpoint: process.env.API_URL,
-  masterKey: process.env.APP_SECRET,
-  port: process.env.PORT
-};
