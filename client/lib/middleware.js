17e264689cede86593e442af19a7df9f48a07710
ScorpIan555 <ian.daley23@gmail.com>
2019-09-16 21:08:40 -0400

    troubleshot Authorization header

--- client/lib/middleware.js
+++ client/lib/middleware.js
@@ -1,10 +1,10 @@
-import { setContext } from 'apollo-link-context';
+// import { setContext } from 'apollo-link-context';
 
-const setAuthorizationLink = setContext((request, previousContext) => ({
-  headers: { authorization: '1234' }
-}));
+// const setAuthorizationLink = setContext((request, previousContext) => ({
+//   headers: { authorization: '1234' }
+// }));
 
-// from apollo-link-context 
-// https://www.apollographql.com/docs/link/links/context/
+// // from apollo-link-context
+// // https://www.apollographql.com/docs/link/links/context/
 
-// https://medium.com/open-graphql/react-hooks-for-graphql-3fa8ebdd6c62
+// // https://medium.com/open-graphql/react-hooks-for-graphql-3fa8ebdd6c62
