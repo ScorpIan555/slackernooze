f1b43477ee7d075afcbd1dcc304e349fa1b0d3cf
ScorpIan555 <ian.daley23@gmail.com>
2019-11-06 22:41:18 -0500

    code cleanup, unneeded package removal, directory org

---
+++ client/lib/graphql/_index.js
@@ -0,0 +1,12 @@
+//  ./client/lib/graphql/_index.js
+import GraphQLMutation from './GraphQLMutation';
+import CREATE_POST_MUTATION from './CREATE_POST_MUTATION';
+import SIGNUP_MUTATION from './SIGNUP_MUTATION';
+import LOGIN_MUTATION from './LOGIN_MUTATION';
+
+export {
+  CREATE_POST_MUTATION,
+  SIGNUP_MUTATION,
+  LOGIN_MUTATION,
+  GraphQLMutation
+};
--- client/lib/graphql/index.js
+++
@@ -1,5 +0,0 @@
-import CREATE_POST_MUTATION from './CREATE_POST_MUTATION';
-import SIGNUP_MUTATION from './SIGNUP_MUTATION';
-import GraphQLMutation from './GraphQLMutation';
-
-export { CREATE_POST_MUTATION, SIGNUP_MUTATION, GraphQLMutation };
