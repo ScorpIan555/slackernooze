8a391d524ca599becdc33cad4efef3bb11363a52
ScorpIan555 <ian.daley23@gmail.com>
2019-11-09 16:44:12 -0500

    moved nav buttons util into components directory

--- client/components/guiUtils/buttons.js
+++ client/components/guiUtils/buttons.js
@@ -1,33 +1,33 @@
-// lib/guiUtils/uttons.js
+// components/guiUtils/uttons.js
 
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faCompass,
   faMapMarkerAlt,
   faUser,
   faShoppingCart
-} from "@fortawesome/free-solid-svg-icons";
+} from '@fortawesome/free-solid-svg-icons';
 
 const navButtons = [
   {
-    label: "Explore",
-    path: "/explore",
+    label: 'Explore',
+    path: '/explore',
     icon: <FontAwesomeIcon icon={faCompass} />
   },
   {
-    label: "Near Me",
-    path: "/nearme",
+    label: 'Near Me',
+    path: '/nearme',
     icon: <FontAwesomeIcon icon={faMapMarkerAlt} />
   },
   {
-    label: "My Cart",
-    path: "/mycart",
+    label: 'My Cart',
+    path: '/mycart',
     icon: <FontAwesomeIcon icon={faShoppingCart} />
   },
   {
-    label: "Profile",
-    path: "/profile",
+    label: 'Profile',
+    path: '/profile',
     icon: <FontAwesomeIcon icon={faUser} />
   }
 ];
