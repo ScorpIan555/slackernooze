398a71a7a63a94d8bdfcf2f7aec9f6b6fb8ce9e5
ScorpIan555 <ian.daley23@gmail.com>
2019-11-12 12:38:11 -0500

    added Signup/login to graphql flow via client

--- client/lib/graphql/gqlComponents/GraphQLMutation.js
+++ client/lib/graphql/gqlComponents/GraphQLMutation.js
@@ -1,14 +1,18 @@
 import { Mutation } from 'react-apollo';
 
-const GraphQLMutation = ({ mutation, variables, handleClick }) => {
-  console.log('props::', variables);
+const GraphQLMutation = ({ mutation, variables, handleAwsCall }) => {
+  console.log('variables::', variables);
   console.log('mutation:::', mutation);
-  // console.log('')
 
   return (
     <Mutation mutation={mutation} variables={variables}>
-      {postMutation => (
-        <button onClick={handleClick}>
+      {handleMutation => (
+        <button
+          onClick={() => {
+            handleAwsCall();
+            handleMutation();
+          }}
+        >
           <p>I killed a tramp once, it made me feelhorny</p>
         </button>
       )}
@@ -17,3 +21,19 @@ const GraphQLMutation = ({ mutation, variables, handleClick }) => {
 };
 
 export default GraphQLMutation;
+
+/*
+while declarative and recommended in the howtographql tut,
+in the future will wanna use the useMutation hook as the Mutation
+component utilizes the render prop pattern, which hooks improve upon..
+
+preferred way to do this in the future would be:
+  // const [createPost, { loading }] = useMutation(POST_MUTATION);
+  let name, email, password;
+  let params = {
+    name,
+    email,
+    password
+  };
+
+*/
